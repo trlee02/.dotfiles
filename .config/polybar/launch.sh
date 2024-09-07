046f995462f4e53444fab9ea9ab7b529634d3fce
Tristan Lee <tristan.rene.lee@gmail.com>
2024-09-07 13:16:04 -0700

    multi-monitor polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
@@ -8,6 +8,14 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
 
 echo "Bars launched..."
